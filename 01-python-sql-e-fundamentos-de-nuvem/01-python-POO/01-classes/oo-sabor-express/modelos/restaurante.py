fbd600b1a6a10f341f83e809ce3545fd3c8d68a8
Rafael <rafasansou1@gmail.com>
2026-01-22 22:24:38 -0300

    feat: adicionando resposta exercicio 1

--- 01-python-sql-e-fundamentos-de-nuvem/01-python-POO/01-classes/oo-sabor-express/modelos/restaurante.py
+++ 01-python-sql-e-fundamentos-de-nuvem/01-python-POO/01-classes/oo-sabor-express/modelos/restaurante.py
@@ -7,7 +7,11 @@ restaurante_praca = Restaurante()
 restaurante_praca.nome = 'Praça'
 restaurante_praca.categoria = 'Comida Caseira'
 restaurante_praca.ativo = True
+
 restaurante_pizza = Restaurante()
 
 restaurantes = [restaurante_praca, restaurante_pizza]
+
+#exercicio 1
+restaurante_praca.categoria = 'Italiana'
 print(vars(restaurante_praca))
