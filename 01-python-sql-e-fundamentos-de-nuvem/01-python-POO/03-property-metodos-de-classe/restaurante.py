e7190c0ebe559066dc68ebc5af32667c72ffd4c4
Rafael <rafasansou1@gmail.com>
2026-01-22 23:54:59 -0300

    feat: adiciona metodo da classe e metodo para alternar status

--- 01-python-sql-e-fundamentos-de-nuvem/01-python-POO/03-property-metodos-de-classe/restaurante.py
+++ 01-python-sql-e-fundamentos-de-nuvem/01-python-POO/03-property-metodos-de-classe/restaurante.py
@@ -10,14 +10,19 @@ class Restaurante:
     def __str__(self):
         return f'Restaurante: {self._nome} | Categoria: {self._categoria} | Status: {self.ativo}'
     
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f'{'Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {"Status"}')
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante._ativo}')
 
     @property
     def ativo(self):
         return 'Ativo' if self._ativo else 'Inativo'
     
+    def alternar_status(self):
+        self._ativo = not self._ativo
+    
 restaurante_praca = Restaurante('praça', 'pomida Caseira')
+restaurante_praca.alternar_status()
 Restaurante.listar_restaurantes()
