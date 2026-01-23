f09cb80efe121f8da82443e84ddba3dfc7acc70f
Rafael <rafasansou1@gmail.com>
2026-01-22 23:24:02 -0300

    feat: adiciona desafio e exercicios

---
+++ 01-python-sql-e-fundamentos-de-nuvem/01-python-POO/02-construtor-instanciando-objetos/musica.py
@@ -0,0 +1,15 @@
+class Musica:
+    def __init__(self, nome='', artista='', duracao=0):
+        self.nome = nome
+        self.artista = artista
+        self.duracao = duracao
+    def __str__(self):
+        return f'Música: {self.nome} | Artista: {self.artista} | Duração: {self.duracao} segundos'
+    
+musica1 = Musica(nome='Under Pressure', artista='Queen & David Bowie', duracao=248)
+musica2 = Musica(nome='The Trooper', artista='Iron Maiden', duracao=245)
+musica3 = Musica(nome='Hotel California', artista='Eagles', duracao=390)
+
+print(musica1)
+print(musica2)
+print(musica3)
