284bab0c2b3a2a791340bdd0d076a1ac1b886846
Rafael <rafasansou1@gmail.com>
2026-01-22 23:14:07 -0300

    feat: adiciona metodos personalizados

--- 01-python-sql-e-fundamentos-de-nuvem/01-python-POO/02-construtor-instanciando-objetos/restaurante.py
+++ 01-python-sql-e-fundamentos-de-nuvem/01-python-POO/02-construtor-instanciando-objetos/restaurante.py
@@ -1,13 +1,20 @@
 class Restaurante:
+    restaurantes = []
+
     def __init__(self, nome, categoria, ativo=False): 
         self.nome = nome
         self.categoria = categoria
         self.ativo = ativo
+        Restaurante.restaurantes.append(self)
+
     def __str__(self):
         return f'Restaurante: {self.nome} | Categoria: {self.categoria} | Ativo: {self.ativo}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'Nome: {restaurante.nome} | Categoria: {restaurante.categoria} | Ativo: {restaurante.ativo}')
 
 restaurante_praca = Restaurante('Praça', 'Comida Caseira')
 restaurante_pizza = Restaurante('Pizza', 'Italiana')
 
-print(restaurante_praca)
-print(restaurante_pizza)
+Restaurante.listar_restaurantes()
